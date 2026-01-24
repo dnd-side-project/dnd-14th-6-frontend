8cf30b1146c5dcb1f859e767d751b2398d330b15
kwaksj329 <kwaksj329@gmail.com>
2026-01-24 16:26:27 +0900

    chore(*): husky, commitlint 설정 추가

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
+    "type-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 100],
+  },
+};
