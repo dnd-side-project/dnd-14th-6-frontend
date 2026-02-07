92d3f886aa82f8884054dba9c0943c09a1b5115b
cindy-chaewon <chaewon1769@daum.net>
2026-02-07 14:14:50 +0900

    feat(report): checkbox 컴포넌트 (#19)

--- src/assets/icons/colored/index.tsx
+++ src/assets/icons/colored/index.tsx
@@ -1,3 +1,5 @@
 export { default as IcArrowLeft } from "./IcArrowLeft";
 export { default as IcArrowRight } from "./IcArrowRight";
+export { default as IcCheckBox } from "./IcCheckBox";
+export { default as IcCheckBoxDefault } from "./IcCheckBoxDefault";
 export { default as IcDeleteGray } from "./IcDeleteGray";
