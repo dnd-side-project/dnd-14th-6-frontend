047028c62406e30f153965ab3692276798225ac3
cindy-chaewon <chaewon1769@daum.net>
2026-02-08 20:09:36 +0900

    feat(report): 리포트 페이지에 적용 (#31)

--- src/app/(main)/report/page.tsx
+++ src/app/(main)/report/page.tsx
@@ -1,15 +1,14 @@
-import Link from "next/link";
 import Flex from "@/components/common/Flex/Flex";
-import * as styles from "./page.css";
+import CumulativeScoreSection from "@/components/report/CumulativeScoreSection/CumulativeScoreSection";
+import { MOCK_SCORE_DETAIL, MOCK_TOTAL_SCORE } from "@/constants/report-mock";
 
 export default function ReportPage() {
   return (
-    <Flex width={"100%"} direction="column">
-      <nav className={styles.nav}>
-        <Link href="/report/ranking" className={styles.link}>
-          랭킹 보기
-        </Link>
-      </nav>
+    <Flex width={"100%"} direction="column" padding={"3.6rem 4.8rem 5rem"}>
+      <CumulativeScoreSection
+        totalScore={MOCK_TOTAL_SCORE}
+        scoreDetail={MOCK_SCORE_DETAIL}
+      />
     </Flex>
   );
 }
