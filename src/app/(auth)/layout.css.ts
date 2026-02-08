f5f28ce83e6e897dff856dcc7073d7f8f60aca3c
chaewon <101500670+cindy-chaewon@users.noreply.github.com>
2026-02-08 13:22:20 +0900

    design(*): backgroundColor 변경 (#15)

--- src/app/(auth)/layout.css.ts
+++ src/app/(auth)/layout.css.ts
@@ -2,7 +2,7 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "@/styles/theme.css";
 
 export const wrapper = style({
-  backgroundColor: vars.color.coolgrey_260,
+  backgroundColor: vars.color.coolgrey_240,
   display: "flex",
   height: "100vh",
 });
