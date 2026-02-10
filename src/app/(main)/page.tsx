d21fff03600776276bac53b58d9037b20027590b
cindy-chaewon <chaewon1769@daum.net>
2026-02-10 14:52:27 +0900

    feat(home): home page에 적용 (#41)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,10 @@
-import Link from "next/link";
 import Flex from "@/components/common/Flex/Flex";
-import * as styles from "./page.css";
+import HomeHeroSection from "@/components/home/HomeHeroSection/HomeHeroSection";
 
 export default function Home() {
   return (
-    <Flex width={"100%"} direction="column">
-      <nav className={styles.nav}>
-        <Link href="/login" className={styles.link}>
-          로그인
-        </Link>
-        <Link href="/game" className={styles.link}>
-          게임 시작
-        </Link>
-      </nav>
+    <Flex width="100%" direction="column" padding={5}>
+      <HomeHeroSection />
     </Flex>
   );
 }
