323f05a20e0d4ce67e865514d41b48179f464c4f
cindy-chaewon <chaewon1769@daum.net>
2026-02-10 18:17:47 +0900

    feat(home): 홈화면 UI (#45)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,32 @@
+import Image from "next/image";
 import Flex from "@/components/common/Flex/Flex";
 import HomeHeroSection from "@/components/home/HomeHeroSection/HomeHeroSection";
 import LogLineSection from "@/components/home/LogLineSection/LogLineSection";
+import * as styles from "./page.css";
 
 export default function Home() {
   return (
-    <Flex width="100%" direction="column" padding={5} gap={15}>
-      <HomeHeroSection />
-      <LogLineSection />
-    </Flex>
+    <div className={styles.pageContainer}>
+      <Image
+        src="/assets/images/home-planet.png"
+        alt=""
+        width={1024}
+        height={844}
+        className={styles.planetImage}
+        priority
+      />
+      <Flex
+        direction="column"
+        paddingLeft={10}
+        paddingRight={10}
+        paddingTop="clamp(12rem, 20vh, 17rem)"
+        paddingBottom={15}
+        gap={25.5}
+        className={styles.content}
+      >
+        <HomeHeroSection />
+        <LogLineSection />
+      </Flex>
+    </div>
   );
 }
