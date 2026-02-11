328a84522f9edfea624928d349ac2da185ced379
chaewon <101500670+cindy-chaewon@users.noreply.github.com>
2026-02-11 23:21:49 +0900

    feat(login): 로그인 페이지 UI (#48)
    * feat(login): 로그인 버튼 컴포넌트 (#47)
    
    * feat(login): 로그인 페이지 UI (#47)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,41 @@
+import Image from "next/image";
 import Flex from "@/components/common/Flex/Flex";
+import Text from "@/components/common/Text/Text";
+import LoginButton from "@/components/login/LoginButton/LoginButton";
+import * as styles from "./page.css";
 
 export default function LoginPage() {
   return (
-    <Flex width={"100%"}>
-      <h1>로그인</h1>
+    <Flex
+      className={styles.pageWrapper}
+      direction="column"
+      align="center"
+      paddingTop={4.6}
+      paddingBottom={5.4}
+    >
+      <Text className={styles.title} variant="display5" color="coolgrey_10">
+        {"지금 바로 로그인하고\n모든 기능을 경험해 보세요!"}
+      </Text>
+      <div className={styles.planetSection}>
+        <Image
+          className={styles.circleImage}
+          src="/assets/images/login-circle.png"
+          alt=""
+          width={534}
+          height={534}
+        />
+        <Image
+          className={styles.planetImage}
+          src="/assets/images/login-planet.png"
+          alt=""
+          width={1639}
+          height={1695}
+        />
+      </div>
+      <Flex direction="column" gap={1.5} align="center">
+        <LoginButton provider="github" />
+        <LoginButton provider="google" />
+      </Flex>
     </Flex>
   );
 }
