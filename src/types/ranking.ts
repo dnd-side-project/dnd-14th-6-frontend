d1965201d2f0db0903a29a525d7ab4a2d30f07cd
cindy-chaewon <chaewon1769@daum.net>
2026-02-13 18:12:04 +0900

    fix(ranking): 타입 분리 (#53)

---
+++ src/types/ranking.ts
@@ -0,0 +1,11 @@
+export interface RankUser {
+  ranking: number;
+  nickname: string;
+  totalScore: string;
+  profileImage: string | null;
+  githubUrl: string | null;
+  tier: {
+    name: string;
+    imageUrl: string;
+  };
+}
