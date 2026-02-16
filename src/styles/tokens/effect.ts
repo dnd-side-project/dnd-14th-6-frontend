59a04b10c0d7350885e81383fb90274055018143
Sujong Kwak <kwaksj329@gmail.com>
2026-02-17 02:33:05 +0900

    feat(game): foul line 컴포넌트 추가 (#63)
    (#62)

--- src/styles/tokens/effect.ts
+++ src/styles/tokens/effect.ts
@@ -1,5 +1,7 @@
 export const effect = {
   game_selected_default: "0px 4px 44px rgba(0, 208, 251, 0.64)",
   game_selected_error: "0px 4px 44px rgba(252, 91, 113, 0.6)",
+  game_deadline_default: "0px 0px 17.2px 0.5px #00fff2",
+  game_deadline_error: "0px 0px 11.4px 0px #ff0000",
   login_github: "inset 2px 2px 7.6px 0px rgba(70, 172, 190, 0.24)",
 } as const;
